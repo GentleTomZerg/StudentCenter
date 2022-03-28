c65d677e946761aa89f6371ed82dd99aa74ede26
gentleTomzerg <chaizztom@163.com>
2022-03-28 14:23:51 +0100

    feat: init springboot project

---
+++ src/main/java/com/man/studentcenter/StudentCenterApplication.java
@@ -0,0 +1,13 @@
+package com.man.studentcenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentCenterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/man/studentcenter/StudentCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.man.studentcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
